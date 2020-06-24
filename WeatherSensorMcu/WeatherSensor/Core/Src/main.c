bb39ef44b62f1fcca86720e74c75534d8080c343
DmitryMI <wet75787@tqosi.com>
2020-06-24 17:28:15 +0300

    Alarm wakeup fixed

--- WeatherSensorMcu/WeatherSensor/Core/Src/main.c
+++ WeatherSensorMcu/WeatherSensor/Core/Src/main.c
@@ -280,7 +280,7 @@ static void MX_RTC_Init(void)
   sAlarm.AlarmDateWeekDaySel = RTC_ALARMDATEWEEKDAYSEL_DATE;
   sAlarm.AlarmDateWeekDay = 0x1;
   sAlarm.Alarm = RTC_ALARM_A;
-  if (HAL_RTC_SetAlarm(&hrtc, &sAlarm, RTC_FORMAT_BCD) != HAL_OK)
+  if (HAL_RTC_SetAlarm_IT(&hrtc, &sAlarm, RTC_FORMAT_BCD) != HAL_OK)
   {
     Error_Handler();
   }
